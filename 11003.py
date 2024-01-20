9f630d966cc4baa10aed3ab2202499914be2a776
shinminchul <su99king@gmail.com>
2024-01-20 22:19:55 +0900

    11003 clear

--- 11003.py
+++ 11003.py
@@ -1,18 +1,27 @@
-from collections import deque
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 
-n, k = map(int, input().split(' '))
+N , L = map(int,input().split())
 
-arr = list(map(int, input().split(' ')))
-q = deque()
-ans = []
-for i in range(n):
-    while q and q[-1][0] > arr[i]:
-        print(q[0])
-        q.pop()
-    while q and q[0][1] < i - k + 1:
-        q.popleft()
-    q.append((arr[i],i))
-    print(q[0][0], end = ' ')
+num_list = list(map(int, input().split()))
+min_list = []
+temp = deque() #해당 범위내의 최솟값,두번째가 될 최솟값 임시저장소
+
+for i in range(N):
+
+    #나가는 값이 최솟값이라면 그거 빼버리셈 ㅇㅇ
+    if i - L >= 0 :
+        if temp[0] == num_list[i-L]:
+            temp.popleft()
+
+    #들어올 값이 최솟값 임시저장소의 원소보다 작다면 기존 원소 삭제시켜버려 ㄱ
+    while len(temp) >= 1 and num_list[i] < temp[-1]:
+        temp.pop()
+
+    temp.append(num_list[i])
+
+    min_list.append(temp[0])
+
+
+print(*min_list)
