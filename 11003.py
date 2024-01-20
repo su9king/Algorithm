07e477c1b3d31e5398bb4b1dd49bd90fb009bcd2
su9king <su99king@gmail.com>
2024-01-20 20:52:46 +0900

    11003

---
+++ 11003.py
@@ -0,0 +1,18 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split(' '))
+
+arr = list(map(int, input().split(' ')))
+q = deque()
+ans = []
+for i in range(n):
+    while q and q[-1][0] > arr[i]:
+        print(q[0])
+        q.pop()
+    while q and q[0][1] < i - k + 1:
+        q.popleft()
+    q.append((arr[i],i))
+    print(q[0][0], end = ' ')
