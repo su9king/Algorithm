2edc48c87071f79dc01776b40859ecc19a19df8d
su9king <su99king@gmail.com>
2024-06-10 20:35:55 +0900

    1927 solve

--- Silver/Data Structure/1927 - 최소 힙.py
+++ Silver/Data Structure/1927 - 최소 힙.py
@@ -1,127 +1,79 @@
 import sys
-count = int(sys.stdin.readline())
-stack = []
-nodes = {}
-
-class Node():
-
-    def __init__(self,number,index):
-
-        self.number = number
-        self.parent = None
-        self.child = []
-        self.index = index
-
-    def addParent(self,parent):
-
-        self.parent = parent
-
-    def addChild(self,child):
-
-        self.child.append(child)
-
-    def removeNode(self,number):
-
-        for i in self.child:
-            nodes[i][0].parent = number
-
-        nodes[number][0].parent = None
-        nodes[number][0].index = 0
-
-def changeNode(highNode,lowNode):
-    nodes[highNode][0].parent, nodes[lowNode][0].parent = lowNode, nodes[highNode][0].parent
-    nodes[highNode][0].child, nodes[lowNode][0].child = nodes[lowNode][0].child, nodes[highNode][0].child
-    nodes[highNode][0].index, nodes[lowNode][0].index = nodes[lowNode][0].index, nodes[highNode][0].index
-    stack[nodes[highNode][0].index], stack[nodes[lowNode][0].index] = stack[nodes[lowNode][0].index], stack[
-        nodes[highNode][0].index]
-
-    for i in nodes[highNode][0].child:
-        nodes[i][0].parent = highNode
+priorityQueue = []
 
 
-def addCheckRelationship(highNode,lowNode):
+def insert(number):
 
-    print("현재 스택 : ",stack , highNode,lowNode)
+    #여기서의 인덱스들은 트리구조 관점임. 실제 리스트에 값 호출시에는 -1 필요.
 
-    if highNode == None: #지금 lowNode 는 RootNode 임. 고로 다 도달했다는 뜻
-        return
-    elif highNode > lowNode: #자식 노드가 더 숫자가 작다면 바꿔야 함.
+    inputIndex = len(priorityQueue) + 1
+    priorityQueue.append(number)
 
-        changeNode(highNode,lowNode)
-
-
-        addCheckRelationship(nodes[lowNode][0].parent, lowNode)
-    else:
-        return
-def removeCheckRelationship(highNode,lowNode):
-
-    print("현재 스택 : ",stack , highNode,lowNode)
-
-    if highNode == None: #지금 lowNode 는 RootNode 임. 고로 다 도달했다는 뜻
-        return
-    elif highNode > lowNode: #자식 노드가 더 숫자가 작다면 바꿔야 함.
-
-        changeNode(highNode,lowNode)
-
-
-        removeCheckRelationship(nodes[highNode][0].child[0], highNode)
-    else:
+    if len(priorityQueue) == 0:
+        priorityQueue.append(number)
         return
 
+    while (inputIndex != 1):
 
-for i in range(count):
+        parentNodeIndex = int(inputIndex / 2)
 
-    number = int(input())
+        if priorityQueue[parentNodeIndex - 1] < priorityQueue[inputIndex - 1]: #넣은 값이 부모 노드보다 크다면
+            return
+        else: #넣은 값이 부모 노드보다 작다면
+            priorityQueue[parentNodeIndex - 1] , priorityQueue[inputIndex - 1] = \
+                priorityQueue[inputIndex - 1] , priorityQueue[parentNodeIndex - 1] #위치 변환 하고 inputIndex 를 변경.
+            inputIndex = parentNodeIndex
+
+def delete():
+
+    if len(priorityQueue) == 0:
+        print(0)
+        return
+    elif len(priorityQueue) == 1:
+        print(priorityQueue.pop())
+        return
+
+    # 여기서의 인덱스들은 트리구조 관점임. 실제 리스트에 값 호출시에는 -1 필요.
+    currentIndex = 1
+    print(priorityQueue[0])
+    priorityQueue[0] = priorityQueue.pop()
+
+    childNodeIndex = currentIndex * 2
+
+    while (currentIndex*2) <= len(priorityQueue):
+
+        childNodeIndex = currentIndex * 2
+
+
+        if childNodeIndex + 1 <= len(priorityQueue): #오른쪽 자식 노드도 있다면,
+
+            if priorityQueue[childNodeIndex -1] > priorityQueue[childNodeIndex]: #자식 노드 비교하여 더 작은 자식을 고름.
+                minChildNodeIndex = childNodeIndex + 1
+            else:
+                minChildNodeIndex = childNodeIndex
+        else:
+            minChildNodeIndex = childNodeIndex
+
+        if priorityQueue[currentIndex - 1] > priorityQueue[minChildNodeIndex - 1]: # 부모노드가 숫자가 더 크다면
+            priorityQueue[currentIndex - 1], priorityQueue[minChildNodeIndex -1] = \
+                priorityQueue[minChildNodeIndex -1] , priorityQueue[currentIndex -1]
+
+            currentIndex = minChildNodeIndex
+
+        else:
+            return
+
+
+## TEST CASE
+chance = int(input())
+
+for i in range(chance):
+    number = int(sys.stdin.readline().rstrip())
 
     if number == 0:
-
-        if len(stack) == 0:
-            print(0)
-        else:
-            minvalue = stack[0]
-            print(minvalue)
-
-            if len(stack) >= 3: #여기까지 완성함 일단.:
-                stack[0] = stack.pop()
-                nodes[minvalue][0].removeNode(stack[0])
-                removeCheckRelationship(stack[0],stack[1])
-            else:
-                del(stack[0])
-
-            del(nodes[minvalue][0])
-
-    elif len(stack) == 0:
-
-        nodes[number] = [Node(number,0)]
-        stack.append(number)
-
+        delete()
     else:
-        index = int(len(stack)/2) # 현재 자식노드를 채워야 하는 노드의 Stack 인덱스
-        new_node = Node(number, len(stack))
-
-        if number in nodes:
-            nodes[number].append(new_node)  # 기존 키에 추가
-        else:
-            nodes[number] = [new_node]  # 새로운 키 생성
-
-
-        stack.append(number)
-        new_node.addParent(stack[index])
-        nodes[stack[index]][-1].addChild(number)
-
-
-        addCheckRelationship(stack[index],number)
-
-
-
-
-
-
-
-
-
-
-
+        insert(number)
 
 
 
